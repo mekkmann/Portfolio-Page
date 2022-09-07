f00e6c0af5bfa58c8f72499d30e1ad8dc1268e45
mekkmann <alex.p.liljekvist@gmail.com>
2022-09-07 23:01:56 +0200

    test fix

--- portfolio-app/components/intro.js
+++ portfolio-app/components/intro.js
@@ -12,12 +12,12 @@ const Intro = () => {
     return (
         <Element name="intro">
             <div className="section intro" >
-                <img class="top-cloud" src="/cloud.png" alt="Image of a Cartoon Cloud" />
+                <img className="top-cloud" src="/cloud.png" alt="Image of a Cartoon Cloud" />
                 <Text h1 size={"5.625rem"} className="color-66bfbf">I'm Pontus Liljekvist</Text>
                 <br />
                 <Text h2 size={"2.5rem"} className="color-66bfbf">Full Stack <span className="underline">Developer</span> </Text>
                 <br />
-                <img class="bottom-cloud" src="/cloud.png" alt="Image of a Cartoon Cloud" />
+                <img className="bottom-cloud" src="/cloud.png" alt="Image of a Cartoon Cloud" />
                 {/* <iframe src="https://giphy.com/embed/RbDKaczqWovIugyJmW" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe> */}
                 
                 <Link to="aboutme" spy={true} smooth={true} >
